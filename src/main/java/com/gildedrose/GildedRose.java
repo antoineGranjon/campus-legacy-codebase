66f8272d342334fa96268af5040d5dc37b6cac7b
antoineGranjon <antoine.granjon@gmail.com>
2019-11-04 15:59:50 +0100

    3 commit

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -16,87 +16,65 @@ public class GildedRose {
 
         for (int i = 0; i < items.length; i++) {
 
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         if (items[i].name.contains("Conjured")) {
                             items[i].quality = items[i].quality - 2;
-                            logger.info("L'item perd 2 de quality");
-                            logger.info("Item : " + items[i].name + " | Quality : " + items[i].quality);
                         } else {
-                            logger.info("Item : " + items[i].name + " | Quality : " + items[i].quality);
                             items[i].quality = items[i].quality - 1;
-                            logger.info("L'item perd 1 de quality");
-                            logger.info("Item : " + items[i].name + " | Quality : " + items[i].quality);
                         }
                     }
                 }
+                logger.info("If quality > 0 || Item : " + items[i].name + " | Quality : " + items[i].quality);
+                logger.info("If quality  > 0  || Item : " + items[i].name + " | SellIn : " + items[i].sellIn);
             } else {
                 if (items[i].quality < 50) {
-                    logger.info("Item : " + items[i].name + " | Quality : " + items[i].quality);
                     items[i].quality = items[i].quality + 1;
-                    logger.info("L'item gagne 1 de quality");
-                    logger.info("Item : " + items[i].name + " | Quality : " + items[i].quality);
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
-                                logger.info("Item : " + items[i].name + " | Quality : " + items[i].quality);
                                 items[i].quality = items[i].quality + 1;
-                                logger.info("L'item gagne 1 de quality");
-                                logger.info("Item : " + items[i].name + " | Quality : " + items[i].quality);
                             }
                         }
 
                         if (items[i].sellIn < 6) {
                             if (items[i].quality < 50) {
-                                logger.info("Item : " + items[i].name + " | Quality : " + items[i].quality);
                                 items[i].quality = items[i].quality + 1;
-                                logger.info("L'item gagne 1 de quality");
-                                logger.info("Item : " + items[i].name + " | Quality : " + items[i].quality);
                             }
                         }
                     }
                 }
+                logger.info("If quality < 50 || Item : " + items[i].name + " | Quality : " + items[i].quality);
+                logger.info("If quality < 50 || Item : " + items[i].name + " | SellIn : " + items[i].sellIn);
             }
 
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                logger.info("Item : " + items[i].name + " | SellIn : " + items[i].sellIn);
                 items[i].sellIn = items[i].sellIn - 1;
-                logger.info("L'item perd 1 de SellIn");
-                logger.info("Item : " + items[i].name + " | SellIn : " + items[i].sellIn);
             }
+            logger.info("If sellIn # sulfuras || Item : " + items[i].name + " | Quality : " + items[i].quality);
+            logger.info("If sellIn # sulfuras || Item : " + items[i].name + " | SellIn : " + items[i].sellIn);
 
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                logger.info("Item : " + items[i].name + " | Quality : " + items[i].quality);
                                 items[i].quality = items[i].quality - 1;
-                                logger.info("L'item perd 1 de quality");
-                                logger.info("Item : " + items[i].name + " | Quality : " + items[i].quality);
                             }
                         }
                     } else {
-                        logger.info("Item : " + items[i].name + " | Quality : " + items[i].quality);
                         items[i].quality = items[i].quality - items[i].quality;
-                        logger.info("L'item perd " + items[i].quality + " de quality");
-                        logger.info("Item : " + items[i].name + " | Quality : " + items[i].quality);
                     }
                 } else {
                     if (items[i].quality < 50) {
-                        logger.info("Item : " + items[i].name + " | Quality : " + items[i].quality);
                         items[i].quality = items[i].quality + 1;
-                        logger.info("L'item gagne 1 de quality");
-                        logger.info("Item : " + items[i].name + " | Quality : " + items[i].quality);
                     }
                 }
-                if(items[i].sellIn < 0){
-                    items[i].sellIn = 0;
-                }
             }
+            logger.info("If sellIn <0 || Item : " + items[i].name + " | Quality : " + items[i].quality);
+            logger.info("If sellIn <0 || Item : " + items[i].name + " | SellIn : " + items[i].sellIn);
         }
     }
 
